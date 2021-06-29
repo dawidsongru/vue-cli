444607107f072356da8bf4545b163036e35cff54
Dawidson Pereira <dawidsongru@gmail.com>
2021-06-29 05:24:10 -0300

    Created Files

---
+++ 02-01-single-file-components(VueServe)/App.vue
@@ -0,0 +1,13 @@
+<template>             
+  <h1>Hello World</h1> <!-- Tudo o que é HTML fica no Template. Template é o único obrigatório. -->
+</template>           
+
+<script>
+export default {  // Tudo o que JavaScript fica no Script
+  
+}
+</script>
+
+<style>
+             /* Tudo o que é CSS fica no Style */
+</style>
